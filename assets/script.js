72a7d998c208a4e4cc66c2a2231cb97de337351b
Kenneth Cheng <itskcheng@gmail.com>
2021-05-07 23:49:16 -0400

    Created the submit functionality to local stroage and to get item. Also got the hours functionality with the switching of the css class depending on the time

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,11 @@ $(document).ready(function () {
     var currentDateArea = $('#currentDay');
     var currentDate = moment();
     currentDateArea.text(currentDate.format('dddd, MMMM Do'));
-});
+    var currentTime = currentDate.format('H') * 1;
+
+    
+
+var inputCounter = 0;
 
 //Using a for loop to create the schedule from 9am - 5pm. 
 
@@ -34,7 +38,7 @@ for (var i = 9; i < 18; i++) {
     //create a p tag for the hours in the div
 
     var time = $('<p>').addClass('time-display');
-    if (timeSchedule <= 12) {
+    if (i <= 12) {
         time.text(timeSchedule + ' AM');
     } else {
         time.text(timeSchedule + 'PM');
@@ -43,16 +47,44 @@ for (var i = 9; i < 18; i++) {
     hourDiv.append(time);
 
     //Create another div with class of col-md-8
-
-    var inputDiv = $('<div/>').addClass('col-md-8 past');
+    var screenTime = ' present';
+    if(i < currentTime){
+    screenTime = ' past'
+    }else if(i > currentTime){
+    screenTime = ' future'
+    }
+    console.log(currentTime);
+    console.log(i);
+    var inputDiv = $('<div/>').addClass('col-md-8' + screenTime);
     rowDiv.append(inputDiv);
 
     //create a input element for the input of the user in the div
 
     var inputField = $('<input/>');
-    inputField.attr({ type: 'text', id: 'text-input', name: 'text' });
+    
+    inputField.attr({ type: 'text', id: 'text-input-' + i, name: 'text', 'data-list': i});
     inputDiv.append(inputField);
 
+    var tester = $('input[data-list]');
+    var testerCount= tester[inputCounter];
+
+    inputCounter++;
+
+    function tru(e){
+
+    e.preventDefault();
+    console.log(e.target);
+    }
+
+
+    //Use execrise 23
+    //Use data attribute to get the time and buttons together to link together.
+
+    /*
+    console.log(textInputs[inputCounter]);
+    var allInputs = textInputs[inputCounter];
+    */
+    //inputCounter++;
 
     //Create another div with class of col-md-2
     var submitDiv = $('<div/>').addClass('col-md-2 saveBtn');
@@ -60,8 +92,53 @@ for (var i = 9; i < 18; i++) {
 
     //create a i tag that is like an icon that is clickable to save in local storage.
     var submitBtn = $('<i class="far fa-save"></i>');
-    submitBtn.attr({ id: 'submit-button' });
+    submitBtn.attr({ id: 'submit-button', 'data-list': i });
+    submitBtn.click(function(){
+        var currValue = $(this).parent().siblings('div.col-md-8').children().val();
+        var dataTime = $(this).attr('data-list');
+        console.log(dataTime);
+        localStorage.setItem(dataTime, currValue);
+
+    });
     submitDiv.append(submitBtn);
-
-
+    var sub = $('i[data-list]');
+   
 }
+    $('#text-input-9').val(localStorage.getItem('9'));
+    $('#text-input-10').val(localStorage.getItem('10'));
+    $('#text-input-11').val(localStorage.getItem('11'));
+    $('#text-input-12').val(localStorage.getItem('12'));
+    $('#text-input-13').val(localStorage.getItem('13'));
+    $('#text-input-14').val(localStorage.getItem('14'));
+    $('#text-input-15').val(localStorage.getItem('15'));
+    $('#text-input-16').val(localStorage.getItem('16'));
+    $('#text-input-17').val(localStorage.getItem('17'));
+
+
+});
+
+//Save the value to local storage. With refresh it will stay there. Gotta put this in a function so it can be run when the button is clicked. 
+
+
+
+
+//Depending on the time the work schedule will have past, present and future. 
+
+/*
+    getInputVal();
+
+    function inputVal(){
+
+        localStorage.setItem('input-answer', JSON.stringify(textInputs.val()));
+    }
+
+    function getInputVal(){
+        var getVal = localStorage.getItem('input-answer');
+
+        if(getVal != null){
+            textInputs.val(getVal);
+        }else{
+            console.log('no answers');
+        }
+    }
+*/
